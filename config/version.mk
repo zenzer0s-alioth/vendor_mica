9a63d55666f69284ca58500a895709e326342254
zenzer0s <praveenzalaki200@gmail.com>
2026-01-23 19:48:17 +0000

    config: Update gms path

--- config/version.mk
+++ config/version.mk
@@ -1,6 +1,6 @@
 # Include gms by default
 ifneq ($(TARGET_EXCLUDE_GMS),true)
-$(call inherit-product, vendor/gms/products/gms.mk)
+$(call inherit-product, vendor/gms/gms_full.mk)
 endif
 
 # Versioning System
